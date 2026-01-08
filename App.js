6e509a9d1c1c24424b9cbd211916facd2cbb8bfd
srikanthgithub2 <srikanthdeveloper25@gmail.com>
2026-01-08 15:07:54 +0530

    new updated code 2

--- App.js
+++ App.js
@@ -1,50 +1,28 @@
-/**
- * 
- * <div id="perent">
- *      <div id="child">
- *          <h2>
- *              iam nested h2 tag
- *           </h2>
- *      </div>
- * </div>
- * 
- * 
- * 
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [
+    React.createElement(
+      "div",
+      { id: "child22", key: "child1" },
+      [
+        React.createElement("h2", { key: "h2-1" }, "iam nested h2 tag"),
+        React.createElement("h3", { key: "h3-1" }, "iam nested h3 tag"),
+      ]
+    ),
+    React.createElement(
+      "div",
+      { id: "child2", key: "child2" },
+      [
+        React.createElement("h2", { key: "h2-2" }, "iam nested h2 tag"),
+        React.createElement("h3", { key: "h3-2" }, "iam nested h3 tag"),
+      ]
+    ),
+  ]
+);
 
-const perent= React.createElement(
-    "div",
-    {id:"perent"},
-    [React.createElement(
-        "div",
-        {id:"child"},
-        [
-        React.createElement("h2",{},"iam nested h2 tag"),
-        React.createElement("h3",{},"iam nested h3 tag")   
-        ]
-      ),
-      React.createElement(
-        "div",
-        {id:"child2"},
-        [
-        React.createElement("h2",{},"iam nested h2 tag"),
-        React.createElement("h3",{},"iam nested h3 tag")   
-        ]
-      )]
-    );
-
-
-
-
-// const heading= React.createElement(
-//     "h1",
-//     {id:"heading"},
-//     "hello react"
-// );
-
-console.log(perent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(perent)
+root.render(parent);
